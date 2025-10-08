91ed822c6b83b6d792294f088d4b89314ec6ee9c
Nilas87 <Nilas87@gmail.com>
2025-10-08 14:50:48 +0000

    now with menu

--- main.py
+++ main.py
@@ -1,18 +1,81 @@
 import pygame
 import sys
+from enum import Enum
 from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from shot import Shot
-def main(num_players):
-    pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    
-    #sets the screen width and height
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+from uielement import UIElement
+
+
+class GameState(Enum):
+    QUIT = -1
+    TITLE = 0
+    GAME = 1
+    GAMETWO = 2
+
+
+def title_screen(screen):
+    op_btn = UIElement(
+        center_position=(640, 300),
+        font_size=30,
+        bg_rgb=BLACK,
+        text_rgb=WHITE,
+        text="One Player",
+        action=GameState.GAME,
+    )
+    tp_btn = UIElement(
+        center_position=(640, 400),
+        font_size=30,
+        bg_rgb=BLACK,
+        text_rgb=WHITE,
+        text="Two Player",
+        action=GameState.GAMETWO,
+    )
+    quit_btn = UIElement(
+        center_position=(640, 500),
+        font_size=30,
+        bg_rgb=BLACK,
+        text_rgb=WHITE,
+        text="Quit",
+        action=GameState.QUIT,
+    )
+     #time keepking
+    clock = pygame.time.Clock()
+    dt = 0
+
+    #groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    asteroidsfield = AsteroidField()
+    buttons = [op_btn, tp_btn, quit_btn]
+
+    while True:
+        mouse_up = False
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                mouse_up = True
+        screen.fill(BLACK)
+        updatable.update(dt)
+        for draw in drawable:
+            draw.draw(screen)
+
+        for button in buttons:
+            ui_action = button.update(pygame.mouse.get_pos(), mouse_up)
+            if ui_action is not None:
+                return ui_action
+            button.draw(screen)
+
+        pygame.display.flip()
+        dt = clock.tick(120) / 1000
+
+def game_screen(screen, num_players):
 
     #time keepking
     clock = pygame.time.Clock()
@@ -43,10 +106,6 @@ def main(num_players):
 
     asteroidsfield = AsteroidField()
 
-
-
-    
-    
     #game loop
     while True:
         for event in pygame.event.get(): #Stops the loop if the windows is closed
@@ -90,15 +149,33 @@ def main(num_players):
         dt = clock.tick(120) / 1000
 
 
+def main():
+    pygame.init()
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    #sets the screen width and height
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_state = GameState.TITLE
+
+    while True:
+        if game_state == GameState.TITLE:
+            game_state = title_screen(screen)
+
+        if game_state == GameState.GAME:
+            game_state = game_screen(screen, 1)
+        
+        if game_state == GameState.GAMETWO:
+            game_state = game_screen(screen, 2)
+
+        if game_state == GameState.QUIT:
+            pygame.quit()
+            return
+
+
 
 
 
 if __name__ == "__main__":
-    print(sys.argv)
-    if len(sys.argv) <= 1:
-        num_player = 1
-    elif sys.argv[1] == "2":
-        num_player = 2
-    else:
-        num_player = 1
-    main(num_player)
+    main()
---
+++ uielement.py
@@ -0,0 +1,106 @@
+import pygame
+from constants import WHITE, BLUE, GREEN, RED, BLACK
+
+def create_surface_with_text(text, font_size, text_rgb, bg_rgb):
+    """ Returns surface with text written on """
+    font = pygame.freetype.SysFont("Courier", font_size, bold=True)
+    surface, _ = font.render(text=text, fgcolor=text_rgb, bgcolor=bg_rgb)
+    return surface.convert_alpha()
+
+class UIElement(pygame.sprite.Sprite):
+    """ An user interface element that can be added to a surface """
+
+    def __init__(self, center_position, text, font_size, bg_rgb, text_rgb, action):
+        """
+        Args:
+            center_position - tuple (x, y)
+            text - string of text to write
+            font_size - int
+            bg_rgb (background colour) - tuple (r, g, b)
+            text_rgb (text colour) - tuple (r, g, b)
+        """
+        self.mouse_over = False  # indicates if the mouse is over the element
+        self.action = action
+        # create the default image
+        default_image = create_surface_with_text(
+            text=text, font_size=font_size, text_rgb=text_rgb, bg_rgb=bg_rgb
+        )
+
+        # create the image that shows when mouse is over the element
+        highlighted_image = create_surface_with_text(
+            text=text, font_size=font_size * 1.2, text_rgb=text_rgb, bg_rgb=bg_rgb
+        )
+
+        # add both images and their rects to lists
+        self.images = [default_image, highlighted_image]
+        self.rects = [
+            default_image.get_rect(center=center_position),
+            highlighted_image.get_rect(center=center_position),
+        ]
+
+        # calls the init method of the parent sprite class
+        super().__init__()
+
+    # properties that vary the image and its rect when the mouse is over the element
+    @property
+    def image(self):
+        return self.images[1] if self.mouse_over else self.images[0]
+
+    @property
+    def rect(self):
+        return self.rects[1] if self.mouse_over else self.rects[0]
+        
+    def update(self, mouse_pos, mouse_up):
+        if self.rect.collidepoint(mouse_pos):
+            self.mouse_over = True
+            if mouse_up:
+                return self.action
+        else:
+            self.mouse_over = False
+
+    def draw(self, surface):
+        """ Draws element onto a surface """
+        surface.blit(self.image, self.rect)
+
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((800, 600))
+
+    # create a ui element
+    uielement = UIElement(
+        center_position=(400, 400),
+        font_size=30,
+        bg_rgb=BLUE,
+        text_rgb=WHITE,
+        text="Hello World",
+        action = None,
+    )
+    quit_btn = UIElement(
+        center_position=(400, 500),
+        font_size=30,
+        bg_rgb=BLUE,
+        text_rgb=WHITE,
+        text="Quit",
+        action=GameState.QUIT,
+    )
+
+    # main loop
+    while True:
+        mouse_up = False
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                mouse_up = True
+        screen.fill(BLUE)
+        uielement.update(pygame.mouse.get_pos(), mouse_up)
+        ui_action = quit_btn.update(pygame.mouse.get_pos(), mouse_up)
+        if ui_action is not None:
+            return
+        uielement.draw(screen)
+        quit_btn.draw(screen)
+        pygame.display.flip()
+
+
+# call main when the script is run
+if __name__ == "__main__":
+    main()
